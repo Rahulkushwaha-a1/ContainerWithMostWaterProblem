3db68d03db48cfef283e5e96b15c10801db75bc7
Rahul KUshwaha <raajkushwaha777@gmail.com>
2025-08-20 09:05:43 +0530

    new program

---
+++ calculatePower(x,n).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1.0;
+        if(x == 0) return 0.0;
+        if(x == 1) return 1.0;
+        if(x == -1 && n%2 == 0) return 1.0;
+        if(x == -1 && n%2 != 0) return -1.0;
+        long  binForm = n;
+        if(n<0){
+…            binForm /=2;
+        }
+        return ans;
+
+        
+    }
+};
